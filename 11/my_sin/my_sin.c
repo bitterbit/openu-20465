3723c8e5ba6d7f023bbabddd9fbbfafc851372c7
Gal Tashma <galtashma@gmail.com>
2021-03-26 15:24:07 +0300

    remove need for pow and factorial functions

--- 11/my_sin/my_sin.c
+++ 11/my_sin/my_sin.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-double abs(double n) {
+double absolute(double n) {
     if (n >= 0) {
         return n;
     }
@@ -9,30 +9,6 @@ double abs(double n) {
     return -n;
 }
 
-double pow(double base, unsigned power) {
-    if (power == 0) {
-        return 1;
-    }
-
-    /* 2^3 = 2*2*2  */
-    double number = 1;
-
-    for(unsigned i=0; i<power; i++) {
-        number *= base;
-    }
-
-    /* printf("pow(%lf, %u) = %lf\n", base, power, number); */
-    return number;
-}
-
-double factorial(unsigned n) {
-    double fact = 1;
-    for (unsigned i=1; i<=n; i++) {
-        fact *= i;
-    }
-
-    return fact;
-}
 
 double my_sin(double x) {
 
@@ -40,22 +16,20 @@ double my_sin(double x) {
     double result = 0;
     int i=0;
     
-    double last_factorial = 1;
+    double factorial = 1;
+    double power = x;
+    int sign = 1;
 
     do {
-        int sign = pow(-1, i);
-        item = pow(x, 2*i+1) / last_factorial;
+        item = power / factorial;
         item = item * sign;
-
-        printf("%lf %lf\n", factorial(2*i+1), last_factorial);
-        /* printf("(%lf / %lf) * %d\n", pow(x, 2*i+1), factorial(2*i+1), sign); */
 
         result += item;
         i++;
-
-        printf("%d %d\n", i*2, i*2+1);
-        last_factorial *= (i*2) * (i*2+1);
-    } while(abs(item) >  0.000001);
+        factorial *= (i*2) * (i*2+1);
+        power *= x*x;
+        sign *= -1;
+    } while(absolute(item) >  0.000001);
 
     return result;
 }
