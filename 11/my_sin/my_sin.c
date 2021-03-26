f54227c43ee64205b618386406ef99c20a85450f
Gal Tashma <galtashma@gmail.com>
2021-03-26 15:18:37 +0300

    made factorial more time efficient

--- 11/my_sin/my_sin.c
+++ 11/my_sin/my_sin.c
@@ -21,7 +21,7 @@ double pow(double base, unsigned power) {
         number *= base;
     }
 
-    printf("pow(%lf, %u) = %lf\n", base, power, number);
+    /* printf("pow(%lf, %u) = %lf\n", base, power, number); */
     return number;
 }
 
@@ -39,16 +39,22 @@ double my_sin(double x) {
     double item;
     double result = 0;
     int i=0;
+    
+    double last_factorial = 1;
 
     do {
         int sign = pow(-1, i);
-        item = pow(x, 2*i+1) / factorial(2*i+1);
+        item = pow(x, 2*i+1) / last_factorial;
         item = item * sign;
 
-        printf("(%lf / %lf) * %d\n", pow(x, 2i+1), factorial(2*i+1), sign);
+        printf("%lf %lf\n", factorial(2*i+1), last_factorial);
+        /* printf("(%lf / %lf) * %d\n", pow(x, 2*i+1), factorial(2*i+1), sign); */
 
         result += item;
         i++;
+
+        printf("%d %d\n", i*2, i*2+1);
+        last_factorial *= (i*2) * (i*2+1);
     } while(abs(item) >  0.000001);
 
     return result;
