b7c5a0d0b05a002b38a9da1ed8a4176d6c01fbb8
Gal Tashma <galtashma@gmail.com>
2021-05-08 14:49:46 +0300

    refactor pormpt line parsing

--- 21/myset.c
+++ 21/myset.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 
 #include "set.h"
 
@@ -12,9 +14,65 @@ Set SETD;
 Set SETE;
 Set SETF;
 
+#define OK 0
+#define ERR_UNDEF_SET_NAME 1
+#define ERR_UNDEF_CMD_NAME 2
+#define ERR_SET_MEMBER_OUT_OF_RANGE 3
+#define ERR_SET_MEMBERS_NOT_TERMINATED 4
+#define ERR_SET_MEMBER_NOT_INT 5
+#define ERR_MISSING_PARAM 6
+#define ERR_EXTRANEOUS_TEXT_AFTER_CMD 7
+#define ERR_MULTIPLE_CONSECUTIVE_COMMAS 8
+#define ERR_MISSING_COMMA 9
+#define ERR_ILLEGAL_COMMA 10
 
-Set* getSet(char* set_name) {
+struct CmdLine {
+    char* cmd;
+    size_t nb_params;
+    char** params;
+};
+
+typedef struct CmdLine CmdLine;
+
+void print_err(int err) {
+    switch(err) {
+        case ERR_UNDEF_SET_NAME:
+            printf("Undefined set name\n");
+            break;
+        case ERR_UNDEF_CMD_NAME:
+            printf("Undefined cmd name\n");
+            break;
+        case ERR_SET_MEMBER_OUT_OF_RANGE:
+            printf("Invalid set member – value out of range\n");
+            break;
+        case ERR_SET_MEMBERS_NOT_TERMINATED:
+            printf("List of set members is not terminated correctly\n");
+            break;
+        case ERR_SET_MEMBER_NOT_INT:
+            printf("Invalid set member – not an integer\n");
+            break;
+        case ERR_MISSING_PARAM:
+            printf("Missing parameter");
+            break;
+        case ERR_EXTRANEOUS_TEXT_AFTER_CMD:
+            printf("Extraneous text after end of command");
+            break;
+        case ERR_MULTIPLE_CONSECUTIVE_COMMAS:
+            printf("Multiple consecutive commas");
+            break;
+        case ERR_MISSING_COMMA:
+            printf("Missing comma");
+            break;
+        case ERR_ILLEGAL_COMMA:
+            printf("Illegal comma");
+            break;
+    }
+}
+
+
+Set* getSet(char* set_name, int *err) {
     if (set_name == NULL) {
+        *err = ERR_UNDEF_SET_NAME;
         return NULL;
     }
 
@@ -39,109 +97,253 @@ Set* getSet(char* set_name) {
         return &SETF;
     }
 
+    *err = ERR_UNDEF_SET_NAME;
     return NULL;
 }
 
-char cmd_delim[3] = " ,";
-
-void readSet(Set *set) {
-    reset_set(set);
-    char *token = NULL;
-    do {
-        token = strtok(NULL, cmd_delim);
-        if (token == NULL) {
-            break;
+bool isNumber(char *s) {
+    size_t len = strlen(s);
+    for (int i=0; i<len; i++) {
+        char c = s[i];
+        if (!isdigit(c) && c != '-') {
+            return false;
         }
-
-        int num = atoi(token);
-        if (num == -1) {
-            break;
-        }
-
-        // don't insert invalid values
-        if (num < 0 || num > 127) {
-            continue;
-        }
-
-        put_set(set, num);
-    } while(token != NULL);
-}
-
-bool handleLine(char *line) {
-    char *cmd = strtok(line, cmd_delim);
-
-    if (cmd == NULL) {
-        return false;
-    }
-
-    if (strcmp(cmd, "stop") == 0) {
-        return false; /* signal to caller that processing of commands should stop */
-    }
-
-    char *set_name = strtok(NULL, cmd_delim);
-    Set *set = getSet(set_name);
-
-    if (set_name == NULL || set == NULL) {
-        printf("Undefined set\n");
-        return false;
-    }
-
-    if (strcmp(cmd, "read_set") == 0) {
-        readSet(set);
-        return true;
-    }
-
-    if (strcmp(cmd, "print_set") == 0) {
-        print_set(set);
-        printf("\n");
-        return true;
-    }
-
-    if ( strcmp(cmd, "union_set") & 
-         strcmp(cmd, "intersect_set") & 
-         strcmp(cmd, "sub_set") & 
-         strcmp(cmd, "symdiff_set") == 0) {
-        printf("Invalid Command\n");
-        return false;
-    }
-
-    // All commands here have three set operands
-    char *set_b_name = strtok(NULL, cmd_delim);
-    Set *set_b = getSet(set_b_name);
-    if (set_b_name == NULL || set_b == NULL) {
-        printf("Undefined set\n");
-        return false;
-    }
-
-    char *set_c_name = strtok(NULL, cmd_delim);
-    Set *set_c = getSet(set_c_name);
-
-    if (set_c_name == NULL || set_c == NULL) {
-        printf("Undefined set\n");
-        return false;
-    }
-
-    if (strcmp(cmd, "union_set") == 0) {
-        union_set(set, set_b, set_c);
-        return true;
-    }
-
-    if (strcmp(cmd, "intersect_set") == 0) {
-        intersect_set(set, set_b, set_c);
-        return true;
-    }
-
-    if (strcmp(cmd, "sub_set") == 0) {
-        sub_set(set, set_b, set_c);
-        return true;
-    }
-
-    if (strcmp(cmd, "symdiff_set") == 0) {
-        symdiff_set(set, set_b, set_c);
-        return true;
     }
 
     return true;
+}
+
+char* skipSpace(char *s)  {
+    char c = *s;
+    while (c != '\0' && isspace(c)) {
+        s++;
+        c = *s;
+    }
+
+    return s;
+}
+
+int readSet(Set *set, CmdLine *line) {
+    reset_set(set);
+    char *token = NULL;
+    int num = 0;
+
+    /* make sure to skip first param as it is the name of the set */
+    for(size_t i=1; i<line->nb_params; i++) {
+        char *token = line->params[i];
+        if (isNumber(token) == false) {
+            return ERR_SET_MEMBER_NOT_INT;
+        }
+
+        num = atoi(token);
+
+        /* terminating character, 
+         * continue parsing to catch errors where there is more data after terminating */
+        if (num == -1) {
+            continue;
+        }
+
+        /* don't insert invalid values */
+        if (num < 0 || num > 127) {
+            return ERR_SET_MEMBER_OUT_OF_RANGE;
+        }
+
+        put_set(set, num);
+    }
+
+    /* last item should be -1 */
+    if (num != -1) {
+        return ERR_SET_MEMBERS_NOT_TERMINATED;
+    }
+
+    return OK;
+}
+
+
+char* lineSkipToParams(char *line) {
+    line = strchr(line, ' ');
+    if (line == NULL) {
+        return NULL;
+    }
+
+    /* skip to after the space */
+    line += 1;
+    /* printf("skipped to params: '%s'\n", line); */
+
+    return line;
+}
+
+char* skipLeadingSpace(char* s) {
+    char c = *s;
+    while(c != '\0' && isspace(c)) {
+        s++;
+        c = *s;
+    }
+
+    return s;
+}
+
+char* nullifyTrailingSpace(char* s) {
+    char* seek = s;
+    char c = *s;
+
+    /* skip all characters up to first space */
+    while(c != '\0' && isspace(c) == false) {
+        seek++;
+        c = *seek;
+    }
+
+    /* we are now pointing to a space or a null 
+     * if is space, replace with null to hint that the string ends here
+     * so trailing spaces will be ignored
+     */
+    if (isspace(c)) {
+        *seek = '\0';
+    }
+
+    return s;
+}
+
+
+CmdLine* parseLine(char *line) {
+    char cmd_delim[2] = " ";
+    char param_delim[2] = ",";
+
+    char* line_duplicate = strdup(line);
+    CmdLine *cmd_line = malloc(sizeof(CmdLine));
+
+    char *cmd = strtok(line, cmd_delim);
+    char *params = lineSkipToParams(line_duplicate);
+
+    cmd_line->cmd = cmd == NULL ? NULL : strdup(cmd);
+    cmd_line->params = NULL;
+    cmd_line->nb_params = 0;
+
+    if (params == NULL) {
+        free(line_duplicate);
+        return cmd_line;
+    }
+
+
+    char* param = strtok(params, param_delim);
+    while(param != NULL) {
+        size_t i = cmd_line->nb_params;
+        cmd_line->nb_params += 1;
+        cmd_line->params = realloc(cmd_line->params, sizeof(*cmd_line->params) * cmd_line->nb_params);
+
+        /* 
+         * skip leading space before duplicating it so it is simple to deallocate later on 
+         * remove trailing space with nulls after duplicating to avoid affecting next strtok calls
+         */
+        param = skipLeadingSpace(param);
+        cmd_line->params[i] = strdup(param);
+        nullifyTrailingSpace(cmd_line->params[i]);
+
+        param = strtok(NULL, param_delim);
+    }
+
+    free(line_duplicate);
+    return cmd_line;
+}
+
+void freeLine(CmdLine *line) {
+    free(line->cmd);
+    for (size_t i=0; i<line->nb_params; i++) {
+        free(line->params[i]);
+    }
+}
+
+void debugLine(CmdLine *line) {
+    printf("$ CMD: %s\n", line->cmd);
+    for (size_t i=0; i<line->nb_params; i++) {
+        printf("$ param[%lu] = %s\n", i, line->params[i]);
+    }
+}
+
+/* 
+ * CMD OP_1, OP_2, OP_3,...
+ * CMD OP_1,OP_2,OP_3,...
+ * CMD OP_1, OP_2 ,OP_3,...
+ * OP can be
+ *   1) name of set
+ *   2) number
+ */
+#define CHECK_ERR(e) if (e != OK) { return e; }
+int handleLine(char *line) {
+    int err = OK;
+
+    CmdLine* cmd_line = parseLine(line);
+    /* debugLine(cmd_line); */
+
+    if (cmd_line->cmd == NULL) {
+        return ERR_UNDEF_CMD_NAME;
+    }
+
+    if (strcmp(cmd_line->cmd, "stop") == 0) {
+        /* TODO signal to caller that processing of commands should stop */
+        return OK; 
+    }
+
+    if (cmd_line->nb_params <= 0) {
+        return ERR_MISSING_PARAM;
+    }
+
+    Set *set = getSet(cmd_line->params[0], &err);
+    CHECK_ERR(err);
+
+    if (strcmp(cmd_line->cmd, "print_set") == 0) {
+        print_set(set);
+        printf("\n");
+        return OK;
+    }
+
+    if (strcmp(cmd_line->cmd, "read_set") == 0) {
+        err = readSet(set, cmd_line);
+        CHECK_ERR(err);
+        return OK;
+    }
+
+    if ( strcmp(cmd_line->cmd, "union_set") & 
+         strcmp(cmd_line->cmd, "intersect_set") & 
+         strcmp(cmd_line->cmd, "sub_set") & 
+         strcmp(cmd_line->cmd, "symdiff_set") == 0) {
+
+        return ERR_UNDEF_CMD_NAME;
+    }
+
+    /* All commands here have three set operands */
+    if (cmd_line->nb_params < 3) {
+        return ERR_MISSING_PARAM;
+    }
+
+    char *set_b_name = cmd_line->params[1];
+    char *set_c_name = cmd_line->params[2]; 
+    if (set_b_name == NULL || set_c_name == NULL) {
+        return ERR_MISSING_PARAM;
+    }
+
+    Set *set_b = getSet(set_b_name, &err);
+    CHECK_ERR(err);
+    Set *set_c = getSet(set_c_name, &err);
+    CHECK_ERR(err);
+
+    if (strcmp(cmd_line->cmd, "union_set") == 0) {
+        union_set(set, set_b, set_c);
+    }
+
+    if (strcmp(cmd_line->cmd, "intersect_set") == 0) {
+        intersect_set(set, set_b, set_c);
+    }
+
+    if (strcmp(cmd_line->cmd, "sub_set") == 0) {
+        sub_set(set, set_b, set_c);
+    }
+
+    if (strcmp(cmd_line->cmd, "symdiff_set") == 0) {
+        symdiff_set(set, set_b, set_c);
+    }
+
+    return OK;
 }
 
 int main(int argc, char **argv) {
@@ -156,11 +358,15 @@ int main(int argc, char **argv) {
     reset_set(&SETE);
     reset_set(&SETF);
 
-    bool should_continue = true;
-    while ((read = getline(&line, &len, stdin)) != -1 && should_continue) {
+    int err = OK;
+    while ((read = getline(&line, &len, stdin)) != -1 && err == OK) {
         line[read - 1] = '\0'; /* Null terminate instead of newline */
         printf(">> %s\n", line);
-        should_continue = handleLine(line);
+        err = handleLine(line);
+    }
+
+    if (err != OK) {
+        print_err(err);
     }
 
     free(line);
